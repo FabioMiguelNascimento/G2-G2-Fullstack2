ce53a0f95a5c7fe8b8232e7de933590fd7cd5408
FabioMiguelNasciment <fabiomnascimento05@gmail.com>
2025-09-03 23:44:51 -0300

    add: adicionado hash de senha e removendo a senha da resposta

--- backend/src/http/controller/auth.controller.ts
+++ backend/src/http/controller/auth.controller.ts
@@ -1,23 +1,33 @@
 import { ConflictError } from "@/error/httpErros.js";
 import AuthRepository from "@/http/repository/auth.repo.js";
 import { RegisterInput } from "@/schema/auth.schema.js";
+import { encodePassword } from "@/utils/bcrypt.js";
 import { NextFunction, Request, Response } from "express";
 
 const repo = new AuthRepository()
 export default class AuthController{
     register = async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const userInput: RegisterInput = req.validatedData
+            let userInput: RegisterInput = req.validatedData
 
             const existingUser = await repo.findUserByEmail(userInput.email)
 
+            // Verifica se ja existe um user com esse email
             if (existingUser) {
                 throw new ConflictError("Usuario ja cadastrado com esse email, que tal fazer login?")
             }
 
-            const newUser = await repo.register(userInput)
+            // Faz o hash na senha do usuario
+            const hashedPassword = encodePassword(userInput.password)
 
-            res.status(200).json({ code: 200, message: "Registro feito com sucesso", data: newUser})
+            userInput = {...userInput, password: hashedPassword}
+
+            let newUser = await repo.register(userInput)
+
+            // Remove a senha da resposta
+            const  {password, ...userWithoutPassword} = newUser
+
+            res.status(200).json({ code: 200, message: "Registro feito com sucesso", data: userWithoutPassword})
         } catch (err) {
             next(err)
         }
