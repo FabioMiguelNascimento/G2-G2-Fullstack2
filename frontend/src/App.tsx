c14e2dff61ee4d9b953a23638d29e77e05e7a228
NataSantos <natasantos1415@gmail.com>
2025-09-09 13:47:28 -0300

    frontend: add: modificado navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from './components/ui/button'
-import Card from './components/card'
 import Header from './components/header'
 import { Outlet } from 'react-router-dom'
-import { NavigationMenu } from '@radix-ui/react-navigation-menu'
 
 function App() {
-  const [count, setCount] = useState(0)
 
    return (
     <>
--- frontend/src/components/card.tsx
+++
@@ -1,6 +0,0 @@
-export default function Card({value,color}){
-    return (
-        <div className={`p-4 bg-${color}`}>{value}</div>
-    )
-
-}
