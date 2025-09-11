c3ae59dd3ef2cbf9d2e9d8634db8c58bd00ad198
vitorotto <vitorotto05@gmail.com>
2025-09-11 12:37:33 -0300

    backend: fix: "strait undefined" função optional do schema de update tava sem os "()". Tudo certo agora :)

--- backend/src/schema/user.schema.ts
+++ backend/src/schema/user.schema.ts
@@ -2,8 +2,8 @@ import z from 'zod';
 import { passwordSchema } from './auth.schema.js';
 
 export const updateSchema = z.object({
-    name: z.string().min(1).optional,
-    email: z.email().optional,
+    name: z.string().min(1).optional(),
+    email: z.email().optional(),
     password: passwordSchema.optional()
 });
 
